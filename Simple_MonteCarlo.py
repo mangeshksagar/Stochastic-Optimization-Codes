261eb5b3906af32ec207eaee768c852e28bbc3b8
mangeshksagar <49077321+mangeshksagar@users.noreply.github.com>
2019-09-10 22:41:20 +0530

    Update Simple_MonteCarlo.py

--- Simple_MonteCarlo.py
+++ Simple_MonteCarlo.py
@@ -31,27 +31,4 @@ def SMC():
     ind = np.argmin(energy)
     x_min = x_val[ind]
     print(x_min)
-#%%    
-def MMC():
-    x_old = np.random.uniform(0.2,2)
-    E1 = f(x_old)
-#%%    
-    energy = []
-    x_val = []
-    for i in np.arange(100):
-        h = 0.1 * np.random.uniform(-0.05,0.05)
-        x_new = x_old + h
-        E2 = f(x_new)
-        if(np.random.uniform(0,1) < np.exp(-(E2-E1))):
-            x_old = x_new    
-            E1 = E2
-            x_val.append(x_old)
-            energy.append(E1)
-            print("x is %f and energy is %f"%(x_new,E2))
-    
-    plt.plot(x_val,energy)
-    plt.show()
-    ind = np.argmin(energy)
-    x_min = x_val[ind]
-    print(x_min)
 #%%
